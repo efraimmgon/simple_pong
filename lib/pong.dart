2301501523e07ebd531cff734143d54d9df55fe6
efraimmgon <efraimmaugusto@gmail.com>
2022-07-07 15:27:12 -0400

    Random speed and direction on bounce

--- lib/pong.dart
+++ lib/pong.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'ball.dart';
 import 'bat.dart';
@@ -12,6 +13,10 @@ class Pong extends StatefulWidget {
 }
 
 class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
+  // Every time the ball bounces, we want to change the value of the random
+  // number based on the border that is reached.
+  double randX = 1; // a random number for the horizontal direction
+  double randY = 1; // a random number for the vertical direction
   double ballDiameter = 50;
   double increment = 5; // ball move speed; higher is faster
   Direction vDir = Direction.down;
@@ -38,7 +43,7 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
 
     animation = Tween<double>(begin: 0, end: 100).animate(controller);
     animation.addListener(() {
-      setState(() {
+      safeSetState(() {
         moveBall();
       });
       checkBorders();
@@ -86,7 +91,7 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   }
 
   void moveBat(DragUpdateDetails update) {
-    setState(() {
+    safeSetState(() {
       batPosition += update.delta.dx;
     });
   }
@@ -94,8 +99,11 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   /// Checks which direction the ball is moving to, and incs or decs to its
   /// current position accordingly.
   void moveBall() {
-    (hDir == Direction.right) ? posX += increment : posX -= increment;
-    (vDir == Direction.down) ? posY += increment : posY -= increment;
+    int incX = (increment * randX).round();
+    (hDir == Direction.right) ? posX += incX : posX -= incX;
+
+    int incY = (increment * randY).round();
+    (vDir == Direction.down) ? posY += incY : posY -= incY;
   }
 
   /// Checks whether the ball has reached its border. and changes the
@@ -104,20 +112,24 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
     // Left
     if (isBallLeft()) {
       hDir = Direction.right;
+      randX = randomNumber();
     }
     // Right
     if (isBallRight()) {
       hDir = Direction.left;
+      randX = randomNumber();
     }
     // Top
     if (isBallTop()) {
       vDir = Direction.down;
+      randY = randomNumber();
     }
     // Bottom
     if (isBallBottom()) {
       // Check if the bat is here, otherwise lose
       if (isBatAlignedWithBall()) {
         vDir = Direction.up;
+        randY = randomNumber();
       } else {
         controller.stop();
         dispose();
@@ -134,11 +146,21 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
       posX >= (batPosition - ballDiameter) &&
       posX <= (batPosition + batWidth + ballDiameter);
 
+  // When we call the dispose method on an object, the object is no longer
+  // usable. Any subsequent call will raise an error. To prevent getting
+  // errors prior to calling the setState method, we check whether the
+  // controller is mounted and active.
   safeSetState(Function f) {
     if (mounted && controller.isAnimating) {
       setState(() {
         f();
       });
     }
+  }
+
+  double randomNumber() {
+    int n = Random().nextInt(101);
+    // a number between 0.5 and 1.5
+    return (50 + n) / 100;
   }
 }
